684bca0dcba24ba25941b6614da3f08c504c2c33
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-04-09 14:09:11 +0900

    finish chapter18

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -7,4 +7,17 @@
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
+
+export default {
+    async asyncData({store}) {
+        if (store.getters['items'].length){
+            return
+        }
+        await store.dispatch('fetchItems')
+    },
+    computed: {
+        ...mapGetters(['items'])
+    }
+}
 </script>
--- store/index.js
+++ store/index.js
@@ -21,20 +21,20 @@ export default () => (new Vuex.Store({
     setUserItems(state, { user, items }) {
       state.userItems[user.id] = items
     },
-    actions: {
-        async fetchItems({ commit }) {
-            const items = await this.$axios.$get('https://qiita.com/api/v2/items?')
-            commit('setItems', { items })
-        },
-        async fetchUserInfo({ commit }, { id }){
-            const [user, items] = await Promise.all([
-                this.$axios.$get(`https://qiita.com/api/v2/users/${id}`),
-                this.$axios.$get(`https://qiita.com/api/v2/items?query=user:${id}`)
-            ])
-        commit('setUser', { user })
-        commit('setUserItems', { user, items })
+  },
+  actions: {
+      async fetchItems({ commit }) {
+          const items = await this.$axios.$get('https://qiita.com/api/v2/items?')
+          commit('setItems', { items })
+      },
+      async fetchUserInfo({ commit }, { id }){
+          const [user, items] = await Promise.all([
+              this.$axios.$get(`https://qiita.com/api/v2/users/${id}`),
+              this.$axios.$get(`https://qiita.com/api/v2/items?query=user:${id}`)
+          ])
+      commit('setUser', { user })
+      commit('setUserItems', { user, items })
 
-      }
     }
   }
 }))
